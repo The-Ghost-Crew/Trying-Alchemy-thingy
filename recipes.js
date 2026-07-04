2ff33f8aebec1807387e4eab9f79625e258fb1ad
Code Ghost <fuentes_ivan0823@icloud.com>
2026-07-03 14:07:11 -1000

    6 more

--- recipes.js
+++ recipes.js
@@ -1,1 +1,7 @@
 recipe("water", "water", "puddle");
+recipe("water", "fire", "steam");
+recipe("water", "air", "mist");
+recipe("water", "earth", "mud");
+recipe("puddle", "puddle", "pond");
+recipe("mud", "earth", "clay");
+recipe("clay", "fire", "brick");
